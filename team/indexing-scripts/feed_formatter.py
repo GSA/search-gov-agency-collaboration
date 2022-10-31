c74c3430a72419b2f540c3b6767584f10fef2029
Amani Farooque <amani.farooque@gmail.com>
2022-10-31 11:09:19 -0400

    Fix typo

--- team/indexing-scripts/feed_formatter.py
+++ team/indexing-scripts/feed_formatter.py
@@ -7,7 +7,7 @@ import feedparser
 ## 1. Create "feeds.txt" file in the "indexing-scripts" folder. Ensure it has all RSS feeds you want to process, with each feed on a new line. 
 ## 2. Ensure all feeds in the file are valid. The script will error if there are spaces in the url, so find-replace " " with "%20".
 ## 3. Open Terminal, and cd into the /indexing-scripts folder
-## 4. Run `python3.9 python_formatter.py`
+## 4. Run `python3.9 feed_formatter.py`
 ## 5. At the end, it will output a file called "rss-feeds.txt" in the "processed" folder. Use Super Admin to bulk upload these URLs.
 
 
